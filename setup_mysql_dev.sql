7fa187711e1d9cd2495e8dcb1002f16e2e08c11a
Mohammed <mrcloud.med@gmail.com>
2024-04-15 21:38:24 +0000

    Update MySQL Setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,15 @@
---- prepares a MySQL server for the project---
---Create the database if it doesn't exist---
+-- prepares a MySQL server for the project
+
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
----Create the user if it doesn't exit---
+
+-- Create the user if it doesn't exit
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
----Grant all privileges on the databse to the user---
-GRANT ALL PRIVILEGE ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
----Grant SELECT privilege on the schema database to the user---
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Grant all privileges on the databse to the user
+GRANT ALL PRIVILEGE ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the schema database to the user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
