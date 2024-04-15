2c332dc5703cb598955b0612fc8871acd283bbf7
Mohammed <mrcloud.med@gmail.com>
2024-04-15 21:41:19 +0000

    Update MySQL

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,10 +6,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the user if it doesn't exit
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on the databse to the user
-GRANT ALL PRIVILEGE ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on the schema database to the user
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on the performance_schema database to the user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
