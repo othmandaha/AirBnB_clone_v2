17e784c03cc8f67f53c410ad6f9934ca8e383fbc
othmandaha <dahaothman02@gmail.com>
2024-04-16 21:46:55 +0100

    fixed to dict

--- models/base_model.py
+++ models/base_model.py
@@ -40,12 +40,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
         try:
             del dictionary["_sa_instance_state"]
         except KeyError:
             pass
         return dictionary
+
 
     def delete(self):
         """Deletes the current istance from storage"""
