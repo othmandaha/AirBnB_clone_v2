548987c8a06e2b00a83abd8f420a961897a16465
Mohammed <mrcloud.med@gmail.com>
2024-04-18 01:51:09 +0000

    update_user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import os
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -10,25 +10,33 @@ from models.base_model import BaseModel, Base
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    password = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    first_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    last_name = Column(
-        String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+    else:
+        email = ''
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        password = Column(String(128), nullable=False)    
+    else:
+        password = ''
+    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        first_name = Column(String(128), nullable=True)    
+    else:
+        first_name = ''
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        last_name = Column(String(128), nullable=True)
+    else:
+        last_name = ''
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship('Place',cascade="all, delete, delete-orphan",
+                              backref='user')
+    else:
+        place = None
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review',cascade="all, delete, delete-orphan",
+                               backref='user')
+    else:
+        reviews = None
