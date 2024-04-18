3ae6404011f5512eac362088349c3a3e4aa8bc5f
Mohammed <mrcloud.med@gmail.com>
2024-04-18 01:28:01 +0000

    Update city/place/review

--- models/city.py
+++ models/city.py
@@ -8,16 +8,17 @@ from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade='all, delete, delete-orphan',
-        backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    """Representation of city """
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        
+        places = relationship(
+            'Place',
+            cascade='all, delete, delete-orphan',
+            backref='cities'
+        )
+    else:
+        name = ''
+        state_id = ''
+        places = None
