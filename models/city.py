21cdbed084528571279df2f1bca7e71a24b0c0ae
Mohammed <mrcloud.med@gmail.com>
2024-04-18 01:34:42 +0000

    Update city/place/review

--- models/city.py
+++ models/city.py
@@ -8,17 +8,15 @@ from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        
-        places = relationship(
-            'Place',
-            cascade='all, delete, delete-orphan',
-            backref='cities'
-        )
-    else:
-        name = ''
-        state_id = ''
-        places = None
+    __tablename__ = 'cities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    state_id = Column(
+        String(60), ForeignKey('states.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade='all, delete, delete-orphan',
+        backref='cities'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
--- models/place.py
+++ models/place.py
@@ -32,33 +32,49 @@ place_amenity = Table(
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-
+    city_id = Column(
+        String(60), ForeignKey('cities.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    description = Column(
+        String(1024), nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    number_rooms = Column(
+        Integer, nullable=False, default=0
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+    number_bathrooms = Column(
+        Integer, nullable=False, default=0
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+    max_guest = Column(
+        Integer, nullable=False, default=0
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+    price_by_night = Column(
+        Integer, nullable=False, default=0
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0
+    latitude = Column(
+        Float, nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
+    longitude = Column(
+        Float, nullable=True
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
+    amenity_ids = []
+    reviews = relationship(
+        'Review',
+        cascade="all, delete, delete-orphan",
+        backref='place'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        amenity_ids = []
-
-        reviews = relationship(
-            'Review',
-            cascade="all, delete, delete-orphan",
-            backref='place'
-        )
-
         amenities = relationship(
             'Amenity',
             secondary=place_amenity,
             viewonly=False,
             backref='place_amenities'
         )
-
     else:
         @property
         def amenities(self):
@@ -73,7 +89,7 @@ class Place(BaseModel, Base):
         @amenities.setter
         def amenities(self, value):
             """Adds an amenity to this Place"""
-            if isinstance(value, Amenity):
+            if type(value) is Amenity:
                 if value.id not in self.amenity_ids:
                     self.amenity_ids.append(value.id)
 
